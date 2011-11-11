28cd3baf1111c524a416ef007fbe577a3f897e76
Mike Fulcher <mike@plan9design.co.uk>
2011-11-11 15:43:45 +0000

    Improvements

--- jquery.leftclick.js
+++ jquery.leftclick.js
@@ -1,16 +1,14 @@
 (function($){
   $.event.special['click:left'] = {
     setup: function() {
-      $.event.add(this, 'click', $.event.special['click:left'].handler, {});
+      $.event.add(this, 'click', $.event.special['click:left'].handler);
     },
     teardown: function() {
-      $.event.remove(this, 'click', $.event.special['click:left'].handler);
+      $.event.remove(this, 'click');
     },
     handler: function(e) {
       if (e.which <= 1 && !e.metaKey && !e.shiftKey && !e.altKey && !e.ctrlKey) {
-        // set correct event type
         e.type = "click:left";
-        // trigger click:left handlers
         $.event.handle.apply(this, arguments);
       }
     }
